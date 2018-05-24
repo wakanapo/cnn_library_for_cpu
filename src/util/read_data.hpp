5e5b7628bb809f1fff270b0b0dc4b1cbc588682b
wakanapo <wakana.tn16@gmail.com>
2018-05-24 16:51:33 +0900

    Change use copy.

--- src/util/read_data.hpp
+++ src/util/read_data.hpp
@@ -207,7 +207,7 @@ Dataset<ImageType, LabelType> ReadCifar10Data(Status st) {
   }
 
   int number_of_images = (st == TRAIN) ? 50000 : 10000;
-  int image_size = 32 * 32 * 3;
+  // int image_size = 32 * 32 * 3;
 
   std::vector<ImageType> images(number_of_images);
   std::vector<LabelType> labels(number_of_images);
@@ -221,7 +221,9 @@ Dataset<ImageType, LabelType> ReadCifar10Data(Status st) {
     }
     labels[i] = OneHot<LabelType>((unsigned long)label);
 
-    ifs.read((char *)&images[i], image_size);
+    std::copy(std::istream_iterator<unsigned char>(ifs),
+              std::istream_iterator<unsigned char>(),
+              images[i].begin());
     if (!ifs) {
       std::cerr << "File read error!" << std::endl;
       exit(1);
