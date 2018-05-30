9956773242d827a2ebac60787a91e92a45a7f9f7
wakanapo <wakana.tn16@gmail.com>
2018-05-30 11:09:06 +0900

    Fixed grap raneg.

--- src/python/make_ga_graph.py
+++ src/python/make_ga_graph.py
@@ -35,7 +35,7 @@ def main(filename, n):
                         c=colors[i])
 
         plt.xlim(-1.0, 1.0)
-        plt.ylim(0, n)
+        plt.ylim(-1, n+1)
         plt.title("genoms")
         plt.ylabel("genoms #")
         plt.xlabel("range")
