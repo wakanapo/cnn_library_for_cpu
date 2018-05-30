963e57ab227ae048113e102bb41e32b67a098225
wakanapo <wakana.tn16@gmail.com>
2018-05-30 11:02:03 +0900

    Change to input genom num from argv.

--- src/python/make_ga_graph.py
+++ src/python/make_ga_graph.py
@@ -47,5 +47,5 @@ if __name__=="__main__":
     if len(argv) != 3:
         print("Usage: Python {} filename #genom".format(argv[0]))
         quit()
-    main(argv[1], argv[2])
+    main(argv[1], int(argv[2]))
 
