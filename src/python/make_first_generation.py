9907c896daee03357252073e96a4214440cb7ab0
wakanapo <wakana.tn16@gmail.com>
2018-05-30 22:29:03 +0900

    Change python code.

--- src/python/make_first_generation.py
+++ src/python/make_first_generation.py
@@ -46,4 +46,8 @@ def main(bit, genom_num):
         f.write(message.SerializeToString())
 
 if __name__ =="__main__":
-    main(4, 3)
+    argv = sys.argv
+    if len(argv) != 3:
+        print("Usage: Python {} bit# genom#".format(argv[0]))
+        quit()
+    main(int(argv[1]), int(argv[2]))
