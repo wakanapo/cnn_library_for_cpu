1f0b900aeb4492cc97f9400625fd067abae5a594
wakanapo <wakana.tn16@gmail.com>
2018-07-22 21:15:40 +0900

    Change dataset.

--- src/python/vgg_like.py
+++ src/python/vgg_like.py
@@ -1,12 +1,12 @@
 from keras import utils
 from keras import optimizers
-from keras.datasets import cifar100
+from keras.datasets import cifar10
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Flatten, Conv2D, MaxPooling2D
 
-(x_train, y_train), (x_test, y_test) = cifar100.load_data(label_mode="fine")
-y_train = utils.to_categorical(y_train, 100)
-y_test = utils.to_categorical(y_test, 100)
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+y_train = utils.to_categorical(y_train, 10)
+y_test = utils.to_categorical(y_test, 10)
 
 model = Sequential()
 model.add(Conv2D(64, (3, 3), padding='same', input_shape=x_train.shape[1:]))
@@ -32,9 +32,9 @@ model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())
 model.add(Dense(1024))
 model.add(Activation('relu'))
-model.add(Dense(1024))
+model.add(Dense(512))
 model.add(Activation('relu'))
-model.add(Dense(100))
+model.add(Dense(10))
 model.add(Activation('sigmoid'))
 
 sgd = optimizers.SGD(lr=0.01)
@@ -45,7 +45,7 @@ x_test = x_test.astype('float32')
 x_train /= 255
 x_test /= 255
 
-model.fit(x_train, y_train, batch_size=100, epochs=10, validation_data=(x_test, y_test), shuffle=True)
+model.fit(x_train, y_train, batch_size=100, epochs=30, validation_data=(x_test, y_test), shuffle=True)
 
 model.save("../../data/vgg_like_models")
 model.save_weights("../../data/vgg_like_params.h5")
