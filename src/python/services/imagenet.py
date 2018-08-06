3514f829b6836f7e4bac82c1848f3fd95305293d
wakanapo <wakana.tn16@gmail.com>
2018-08-06 18:46:01 +0900

    Add debug comment.

--- src/python/services/imagenet.py
+++ src/python/services/imagenet.py
@@ -2,6 +2,7 @@ import re
 import os
 import numpy as np
 from keras.preprocessing.image import img_to_array, load_img
+from tqdm import tqdm
 
 def list_pictures(directory, ext='JPEG|jpg|jpeg|bmp|png|ppm'):
     return [os.path.join(root, f)
@@ -14,7 +15,7 @@ def load():
         labels = np.asarray(labels)
 
     imgs = []
-    for picture in list_pictures("data/ILSVRC2012/val/"):
+    for picture in tqdm(list_pictures("data/ILSVRC2012/val/")):
         img = img_to_array(load_img(picture, target_size=(224, 224)))
         imgs.append(img)
     return imgs, labels
