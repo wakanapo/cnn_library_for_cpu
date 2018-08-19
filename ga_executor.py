36a77563b229f766ba8e876733c0e23f793a32a0
wakanapo <wakana.tn16@gmail.com>
2018-08-19 14:42:27 +0900

    Change read to non blocking read.

--- ga_executor.py
+++ ga_executor.py
@@ -1,18 +1,32 @@
 import sys
 import subprocess
+from fcntl import fcntl, F_SETFL, F_GETFL
+from os import O_NONBLOCK
+
+def non_blocking_read(output):
+    fd = output.fileno()
+    fl = fcntl(fd, F_GETFL)
+    fcntl(fd, F_SETFL, fl | O_NONBLOCK)
+    try:
+        return output.read()
+    except:
+        return ""
 
 def run(genom_name):
     server = subprocess.Popen('python src/python/services/genom_evaluation_server.py',
                             shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     while True:
-        s_line = server.stdout.readline()
+        s_line = non_blocking_read(server.stdout)
         if s_line:
             sys.stdout.write(s_line.decode('utf-8'))
             if s_line.decode('utf-8') == 'ready\n':
                 client = subprocess.Popen('./bin/ga {}'.format(genom_name), shell=True,
                                           stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
                 while True:
-                    c_line = client.stdout.readline()
+                    s_line = non_blocking_read(server.stdout)
+                    c_line = non_blocking_read(client.stdout)
+                    if s_line:
+                        sys.stdout.write(s_line.decode('utf-8'))
                     if c_line:
                         sys.stdout.write(c_line.decode('utf-8'))
                     if not c_line and client.poll() is not None:
