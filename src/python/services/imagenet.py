c38a729036af1450e7a4ee1a1334917481e33068
wakanapo <wakana.tn16@gmail.com>
2018-08-20 22:20:04 +0900

    Change load imagenet.

--- src/python/services/imagenet.py
+++ src/python/services/imagenet.py
@@ -1,14 +1,15 @@
 import re
 import os
+import scipy.io
 import numpy as np
 from keras.preprocessing.image import img_to_array, load_img
 from keras.utils import np_utils
 from tqdm import tqdm
 
 def load():
+    matdata = scipy.io.loadmat("data/ILSVRC2012/ILSVRC2012_devkit_t12/data/meta.mat")
     with open("data/ILSVRC2012/ILSVRC2012_devkit_t12/data/ILSVRC2012_validation_ground_truth.txt", 'r') as f:
-        labels = np.asarray([int(x) for x in f])
-        labels = np_utils.to_categorical(labels)
+        labels = [matdata['synsets'][int(x)-1]['WNID'][0][0] for x in f]
 
     imgs = []
     for i in tqdm(range(len(labels))):
